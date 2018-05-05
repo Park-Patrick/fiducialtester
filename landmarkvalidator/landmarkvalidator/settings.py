71f615fde9d95099083445ee139bbf15b13ccd7e
Olivia Stanley <ostanle2@uwo.ca>
2018-05-05 18:34:37 -0400

    first deployed commit

--- landmarkvalidator/landmarkvalidator/settings.py
+++ landmarkvalidator/landmarkvalidator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h7p)mh^l-$o*%)j#mogoj*)jo$@y3j85f$#qaj2_bzxow4hese'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
