c627eea7f59a4b92a0eb9a058eb2a73a5808df48
Olivia Stanley <ostanle2@uwo.ca>
2018-05-05 15:12:05 -0400

    csv full fixes and added gitignore for spyder

--- webapp/leftrightflip.py
+++ webapp/leftrightflip.py
@@ -6,7 +6,7 @@ Created on Sat May  5 14:33:12 2018
 """
 from csv_test import import_csv
 
-filename1 = "D:/brainhack2018/fiducialtester/webapp/uploads/UHF_MEAN.fcsv"
-filename2 = "D:/brainhack2018/fiducialtester/webapp/uploads/UHF_MEAN_no_outliers.fcsv"
+filename1 = "./uploads/UHF_MEAN.fcsv"
+filename2 = "./uploads/UHF_MEAN_no_outliers.fcsv"
 
 xyz_planned, xyz_actual, euclid_dist = import_csv(filename1, filename2)
