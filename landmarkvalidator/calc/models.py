be5f70c55a325629e00c3374cfcf91f6d6cb7b8e
Olivia Stanley <ostanle2@uwo.ca>
2018-05-05 19:31:25 -0400

    local database working, not deployed

--- landmarkvalidator/calc/models.py
+++ landmarkvalidator/calc/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+#from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class PointSet(models.Model):
+    UUID = models.UUIDField(max_length=200)
+    date = models.DateTimeField('Date of landmarking')
+    session = models.IntegerField()
+    subject = models.CharField(max_length=200)
+    template = models.FileField(upload_to=None, max_length=200)
+#    error = ArrayField(models.FloatField(), size=None)
+#    point = ArrayField(models.FloatField(), size=None)
+    
+class Fiducial(models.Model):
+    PUID = models.UUIDField(max_length=200)
+    location = models.CharField(max_length=1)
+    pair = models.IntegerField()
+    template = models.FileField(upload_to=None, max_length=200)
