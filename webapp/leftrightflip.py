2f801551f7ac14f452ab24b86fbd57822aded1cb
daniel <dcao6@uwo.ca>
2018-05-05 15:54:36 -0400

    LR changes

--- webapp/leftrightflip.py
+++ webapp/leftrightflip.py
@@ -10,3 +10,11 @@ filename1 = "D:/brainhack2018/fiducialtester/webapp/uploads/UHF_MEAN.fcsv"
 filename2 = "D:/brainhack2018/fiducialtester/webapp/uploads/UHF_MEAN_no_outliers.fcsv"
 
 xyz_planned, xyz_actual, euclid_dist = import_csv(filename1, filename2)
+
+for i in euclid_dist:
+    if i > 0:
+        values = print(i)
+
+    
+        
+        
