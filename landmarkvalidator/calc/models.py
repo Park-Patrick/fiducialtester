b6728d14dbcc4a0a9fbdb55c4adda8e10de20a4a
Olivia Stanley <ostanle2@uwo.ca>
2018-05-06 10:36:22 -0400

    file based database for error and points

--- landmarkvalidator/calc/models.py
+++ landmarkvalidator/calc/models.py
@@ -1,18 +1,27 @@
 from django.db import models
-#from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
 class PointSet(models.Model):
+    # UUID for pointset
     UUID = models.UUIDField(max_length=200)
+    # Date of upload
     date = models.DateTimeField('Date of landmarking')
+    # Session for user (mannually specified)
     session = models.IntegerField()
+    # Subject
     subject = models.CharField(max_length=200)
-    template = models.FileField(upload_to=None, max_length=200)
-#    error = ArrayField(models.FloatField(), size=None)
-#    point = ArrayField(models.FloatField(), size=None)
+    # Template brain
+    template = models.CharField(max_length=200)
+    # 4 by n array of x, y, z, and euclidean error
+    error = models.FileField(upload_to=None, max_length=200)
+    # pointset of fiducials
+    point = models.FileField(upload_to=None, max_length=200)
     
 class Fiducial(models.Model):
+    # UUID for fiducial
     PUID = models.UUIDField(max_length=200)
+    # Location (medial, right, or left)
     location = models.CharField(max_length=1)
+    # If right or left the pair point, if not -1
     pair = models.IntegerField()
-    template = models.FileField(upload_to=None, max_length=200)
+    # Template brain
+    template = models.CharField(max_length=200)
